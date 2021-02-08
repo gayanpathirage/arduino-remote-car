66a604d2f82db5dab2ea943da85ca9d58fa1e79e
Gayan Pathirage <gayanpathirage@gmail.com>
2021-02-08 07:10:59 +0530

    Update CAR.ino
    MIT License

--- CAR.ino
+++ CAR.ino
@@ -1,5 +1,25 @@
 /*
- *
+MIT License
+
+Copyright (c) 2021 Gayan Pathirage
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.MIT License
  */
 
 #define DECODE_NEC          1         // etc. see IRremote.h
@@ -41,10 +61,9 @@ void loop() {
     if (IrReceiver.decode()) {
 
         // Print a short summary of received data
-        //        IrReceiver.printIRResultShort(&Serial);
-        //        Serial.print("......... >");
-        //        Serial.print(IrReceiver.decodedIRData.rawDataPtr->rawbuf[0] * MICROS_PER_TICK);
-        //        Serial.println();
+        // Uncomment when debugging 
+        // IrReceiver.printIRResultShort(&Serial);
+
 
         /*
          * !!!Important!!! Enable receiving of the next value,
